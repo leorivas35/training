33a506daf1c7d2413eb557facf04b77bb9e5018f
Leo Rivas <agorarivas1762@gmail.com>
2026-06-26 06:39:30 -0500

    InitialCommit

---
+++ .NET-Angular/DatingApp/API/Program.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AddDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#endregion
+var app = builder.Build();
+
+#region Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+#endregion
