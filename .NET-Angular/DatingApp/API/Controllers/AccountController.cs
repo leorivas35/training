b37706d6feb2e9a75289d84c22ba890338f219c4
Leo Rivas <agorarivas1762@gmail.com>
2026-07-18 11:28:58 -0500

    End of section 5

--- .NET-Angular/DatingApp/API/Controllers/AccountController.cs
+++ .NET-Angular/DatingApp/API/Controllers/AccountController.cs
@@ -1,12 +1,13 @@
-﻿using API.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace API.Controllers
 {
--- .NET-Angular/DatingApp/client/src/app/app.ts
+++ .NET-Angular/DatingApp/client/src/app/app.ts
@@ -1,26 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { lastValueFrom } from 'rxjs';
+import { Home } from "../feature/home/home";
+import { User } from '../types/user';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = signal('client');
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
-  ngOnInit(): void {
-   this.http.get<any[]>('https://localhost:5001/api/members').subscribe({
-    next: response => {
-      console.log(response);
-      this.members.set(response);
-    },
-    error: error => console.log(error),
-    complete: () => console.log('Request completed')
-   })
+  async ngOnInit() {
+   this.members.set(await this.getMembers());
+   this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  async getMembers(){
+    try {
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
+      
+    }
   }
 }
